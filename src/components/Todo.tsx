d24db67d657c9b58b72d82e521a98f94caa31c88
enskerti <enskerti@gmail.com>
2024-08-05 11:15:35 +0300

    completed

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoMdRemoveCircleOutline } from "react-icons/io";
 import { FaCheck } from "react-icons/fa";
 import { FaRegEdit } from "react-icons/fa";
+import { TodoType } from "../types/Types";
+import { useDispatch } from "react-redux";
+import { removeTodoById, updateTodo } from "../redux//todoSlice";
 
-const Todo = () => {
+interface TodoProps {
+  todoProps: TodoType;
+}
+
+const Todo = ({ todoProps }: TodoProps) => {
+  const { id, content } = todoProps;
+
+  const dispatch = useDispatch();
+  const [editable, setEditable] = useState<boolean>(false);
+  const [newTodo, setNewTodo] = useState<string>(content);
+
+  const handleRemove = () => {
+    dispatch(removeTodoById(id));
+  };
+
+  const handleIpdateTodo = () => { 
+    const payload = {
+      id:id,
+      content:newTodo
+    }
+    dispatch(updateTodo(payload))
+    setEditable(false)
+   }
+
   return (
     <div
       style={{
@@ -14,13 +40,37 @@ const Todo = () => {
         border: "1px solid lightgrey",
         padding: "16px",
         marginTop: "15px",
-        borderRadius:"5px"
+        borderRadius: "5px",
       }}
     >
-      <div>Ben ilk todo'yum</div>
+      {editable ? (
+        <input
+          type="text"
+          style={{
+            width: "400px",
+            border: "none",
+            borderBottom: "1px solid lightgrey",
+            outline: "none",
+          }}
+          value={newTodo}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setNewTodo(e.target.value)
+          }
+        />
+      ) : (
+        <div>{content}</div>
+      )}
       <div>
-        <IoMdRemoveCircleOutline  className="icons"  style={{marginRight:"8px"}}/>
-        <FaRegEdit  className="icons" />
+        <IoMdRemoveCircleOutline
+          className="icons"
+          style={{ marginRight: "8px" }}
+          onClick={handleRemove}
+        />
+        {editable ? (
+          <FaCheck className="icons" onClick={handleIpdateTodo} />
+        ) : (
+          <FaRegEdit className="icons" onClick={() => setEditable(true)} />
+        )}
       </div>
     </div>
   );
--- src/components/TodoCreate.tsx
+++ src/components/TodoCreate.tsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createTodo } from "../redux/todoSlice";
+import { TodoType } from "../types/Types";
 
 const TodoCreate = () => {
-  return (
-    <div className='todo-create'>
-        <input type="text" placeholder='Todo giriniz...' className='todo-input'/>
-        <button className='todo-create-button'>Oluştur</button>
-    </div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default TodoCreate
+  const [newTodo, setNewTodo] = useState<string>("");
+
+  const handleCreateTodo = () => {
+    if(newTodo.trim().length == 0){
+      alert("todo giriniz!")
+    }
+
+    const payload :TodoType = {
+      id:Math.floor(Math.random()*999999999),
+      content:newTodo
+    }
+    dispatch(createTodo(payload))
+    setNewTodo("");
+  };
+
+  return (
+    <div className="todo-create">
+      <input
+        type="text"
+        placeholder="Todo giriniz..."
+        className="todo-input"
+        value={newTodo}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setNewTodo(e.target.value)
+        }
+      />
+      <button 
+        className="todo-create-button" 
+        onClick={handleCreateTodo}>
+        Oluştur
+      </button>
+    </div>
+  );
+};
+
+export default TodoCreate;
